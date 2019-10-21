aab1be21cb4762a02a3b17c864b344d48c6e9087
pandu cahyo pangestu <pandusudo@gmail.com>
2019-10-21 20:51:44 +0700

    Add login screen

--- src/Screens/Account.js
+++ src/Screens/Account.js
@@ -1,10 +1,29 @@
 import React from 'react'
-import { Text } from 'native-base'
+import { Container, View, Header, Left, Content, Body, Right, Button, Icon, Title, Text } from 'native-base'
 
 export default ({ navigation }) => {
     return (
         <>
-            <Text>I'm from Account Navigation</Text>
+        <Header>
+          <Left>
+            <Button transparent>
+              <Icon name='arrow-back' />
+            </Button>
+          </Left>
+          <Body>
+            <Title>Masuk</Title>
+          </Body>
+          <Right>
+            <Button transparent>
+              <Icon name='more' />
+            </Button>
+          </Right>
+        </Header>
+        <Content>
+          <View style={{padding: 20}}>
+            <Text>Log in untuk memesan lebih cepat, dan mendapat promo eksklusif lainnya</Text>
+          </View>
+        </Content>
         </>
     )
 }
