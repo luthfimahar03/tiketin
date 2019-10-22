1838cf282782da34f796fcd751681126b0a7f7c1
Sutan Nasution <sutan.gnst@gmail.com>
2019-10-22 22:04:59 +0700

    cleaning account screen

--- src/Screens/Account/index.js
+++ src/Screens/Account/index.js
@@ -47,7 +47,7 @@ export default ({ navigation }) => {
         }
     }
 
-    const validateEmail = (email) => {
+    const validateEmail = email => {
         var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
         if (!re.test(email)) {
@@ -62,13 +62,13 @@ export default ({ navigation }) => {
         }
     }
 
-    const validatePassword = (password) => {
-        if(!password.match(/(\d+)/)) {
+    const validatePassword = password => {
+        if (!password.match(/(\d+)/)) {
             setErrorPassword('Password should at least contain 1 number')
-            return false;
+            return false
         } else if (password.length < 4) {
             setErrorPassword('Password should at least have 4 character')
-        	return false;
+            return false
         } else {
             setErrorPassword('')
             return true
@@ -77,153 +77,162 @@ export default ({ navigation }) => {
 
     return (
         <>
-        <Header
-            title="Masuk"
-            leftComponent={
-                <Button transparent onPress={() => navigation.goBack()}>
-                    <Icon name="arrow-back" />
-                </Button>
-            }
-            />
-        <Content>
-            <View style={{ paddingVertical: 15, paddingHorizontal: 30 }}>
-                <Text style={styles.titleStyle}>
-                    Log in untuk memesan lebih cepat, dan mendapat promo
-                    eksklusif lainnya
-                </Text>
-                <Form>
-                    <Item rounded style={styles.inputStyle} error={ErrorEmail.length > 0}>
-                        <Input placeholder="example@example.com"
-                            onChangeText={text => setEmail(text)}
-                        />
-                    </Item>
-                    <Item rounded style={styles.inputStyle} error={ErrorPassword.length > 0}>
-                        <Input
-                            placeholder="********"
-                            secureTextEntry={SecureText}
-                            onChangeText={text => setPassword(text)}
-                        />
-                        <Button transparent onPress={() => hideUnhide()}>
-                            <Icon
-                                type="Feather"
-                                name={IconName}
-                                style={{ color: '#a9a9a9' }}
-                                />
-                        </Button>
-                    </Item>
-                </Form>
-                <View
-                    style={{
-                        flex: 1,
-                        flexWrap: 'wrap',
-                        paddingHorizontal: 20,
-                        flexDirection: 'row',
-                        justifyContent: 'flex-end'
-                    }}
-                    >
-                    <View>
-                        <Text style={{ fontSize: 14, color: '#0653ff' }}>
-                            Lupa kata sandi?
-                        </Text>
-                    </View>
-                </View>
-                <View>
-                    <Button
-                        rounded
-                        onPress={() => onSubmit()}
-                        style={{
-                            justifyContent: 'center',
-                            elevation: 0,
-                            backgroundColor: '#FBDE06',
-                            marginVertical: 25,
-                            padding: 10
-                        }}
-                        >
-                        <Text style={{ fontSize: 18, color: '#0164D3' }}>
-                            Login
-                        </Text>
+            <Header
+                title="Masuk"
+                leftComponent={
+                    <Button transparent onPress={() => navigation.goBack()}>
+                        <Icon name="arrow-back" />
                     </Button>
-                </View>
-                <View
-                    style={{
-                        flex: 1,
-                        flexWrap: 'wrap',
-                        marginVertical: 10,
-                        flexDirection: 'row',
-                        justifyContent: 'center',
-                        alignItems: 'flex-end'
-                    }}
+                }
+            />
+            <Content>
+                <View style={{ paddingVertical: 15, paddingHorizontal: 30 }}>
+                    <Text style={styles.titleStyle}>
+                        Log in untuk memesan lebih cepat, dan mendapat promo
+                        eksklusif lainnya
+                    </Text>
+                    <Form>
+                        <Item
+                            rounded
+                            style={styles.inputStyle}
+                            error={ErrorEmail.length > 0}
+                        >
+                            <Input
+                                placeholder="example@example.com"
+                                onChangeText={text => setEmail(text)}
+                            />
+                        </Item>
+                        <Item
+                            rounded
+                            style={styles.inputStyle}
+                            error={ErrorPassword.length > 0}
+                        >
+                            <Input
+                                placeholder="********"
+                                secureTextEntry={SecureText}
+                                onChangeText={text => setPassword(text)}
+                            />
+                            <Button transparent onPress={() => hideUnhide()}>
+                                <Icon
+                                    type="Feather"
+                                    name={IconName}
+                                    style={{ color: '#a9a9a9' }}
+                                />
+                            </Button>
+                        </Item>
+                    </Form>
+                    <View
+                        style={{
+                            flex: 1,
+                            flexWrap: 'wrap',
+                            paddingHorizontal: 20,
+                            flexDirection: 'row',
+                            justifyContent: 'flex-end'
+                        }}
                     >
+                        <View>
+                            <Text style={{ fontSize: 14, color: '#0653ff' }}>
+                                Lupa kata sandi?
+                            </Text>
+                        </View>
+                    </View>
                     <View>
-                        <Text style={{ color: 'grey' }}>atau dengan</Text>
+                        <Button
+                            rounded
+                            onPress={() => onSubmit()}
+                            style={{
+                                justifyContent: 'center',
+                                elevation: 0,
+                                backgroundColor: '#FBDE06',
+                                marginVertical: 25,
+                                padding: 10
+                            }}
+                        >
+                            <Text style={{ fontSize: 18, color: '#0164D3' }}>
+                                Login
+                            </Text>
+                        </Button>
+                    </View>
+                    <View
+                        style={{
+                            flex: 1,
+                            flexWrap: 'wrap',
+                            marginVertical: 10,
+                            flexDirection: 'row',
+                            justifyContent: 'center',
+                            alignItems: 'flex-end'
+                        }}
+                    >
+                        <View>
+                            <Text style={{ color: 'grey' }}>atau dengan</Text>
+                        </View>
+                    </View>
+                    <View
+                        style={{
+                            flex: 1,
+                            flexWrap: 'wrap',
+                            marginVertical: 30,
+                            flexDirection: 'row',
+                            justifyContent: 'center'
+                        }}
+                    >
+                        <Card style={{ width: '95%' }}>
+                            <CardItem>
+                                {/*<Icon active name="logo-google" />*/}
+                                <Image
+                                    source={google}
+                                    style={{
+                                        width: 20,
+                                        height: 20,
+                                        marginRight: 10
+                                    }}
+                                />
+                                <Text>Google</Text>
+                            </CardItem>
+                            <CardItem>
+                                <View
+                                    style={{
+                                        alignSelf: 'stretch',
+                                        width: '100%',
+                                        borderBottomColor: '#9f9f9f',
+                                        backgroundColor: 'red',
+                                        borderBottomWidth:
+                                            StyleSheet.hairlineWidth
+                                    }}
+                                />
+                            </CardItem>
+                            <CardItem>
+                                {/*<Icon active name="logo-facebook" />*/}
+                                <Image
+                                    source={facebook}
+                                    style={{
+                                        width: 20,
+                                        height: 20,
+                                        marginRight: 10
+                                    }}
+                                />
+                                <Text>Facebook</Text>
+                            </CardItem>
+                        </Card>
+                    </View>
+                    <View
+                        style={{
+                            flex: 1,
+                            flexWrap: 'wrap',
+                            flexDirection: 'row',
+                            justifyContent: 'center',
+                            alignItems: 'flex-end'
+                        }}
+                    >
+                        <View>
+                            <Text>
+                                Belum punya akun?{' '}
+                                <Text style={{ color: '#0164D3' }}>DAFTAR</Text>
+                            </Text>
+                        </View>
                     </View>
                 </View>
-                <View
-                    style={{
-                        flex: 1,
-                        flexWrap: 'wrap',
-                        marginVertical: 30,
-                        flexDirection: 'row',
-                        justifyContent: 'center'
-                    }}
-                    >
-                    <Card style={{ width: '95%' }}>
-                        <CardItem>
-                            {/*<Icon active name="logo-google" />*/}
-                            <Image
-                                source={google}
-                                style={{
-                                    width: 20,
-                                    height: 20,
-                                    marginRight: 10
-                                }}
-                                />
-                            <Text>Google</Text>
-                        </CardItem>
-                        <CardItem>
-                            <View
-                                style={{
-                                    alignSelf: 'stretch',
-                                    width: '100%',
-                                    borderBottomColor: '#9f9f9f',
-                                    backgroundColor: 'red',
-                                    borderBottomWidth:
-                                    StyleSheet.hairlineWidth
-                                }}
-                                />
-                        </CardItem>
-                        <CardItem>
-                            {/*<Icon active name="logo-facebook" />*/}
-                            <Image
-                                source={facebook}
-                                style={{
-                                    width: 20,
-                                    height: 20,
-                                    marginRight: 10
-                                }}
-                                />
-                            <Text>Facebook</Text>
-                        </CardItem>
-                    </Card>
-                </View>
-                <View
-                    style={{
-                        flex: 1,
-                        flexWrap: 'wrap',
-                        flexDirection: 'row',
-                        justifyContent: 'center',
-                        alignItems: 'flex-end'
-                    }}
-                    >
-                    <View>
-                        <Text>
-                            Belum punya akun?{' '}
-                            <Text style={{ color: '#0164D3' }}>DAFTAR</Text>
-                        </Text>
-                    </View>
-                </View>
-            </View>
-        </Content>
+            </Content>
         </>
 )
 }
