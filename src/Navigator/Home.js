238d38c93ee9894d6ff0886c94dd5e72efc94533
pandu cahyo pangestu <pandusudo@gmail.com>
2019-10-24 19:52:58 +0700

    Form Search Mobil added

--- src/Navigator/Home.js
+++ src/Navigator/Home.js
@@ -3,12 +3,14 @@ import { createStackNavigator } from 'react-navigation-stack'
 import Home from '../Screens/Home'
 import FormSearchHotel from '../Screens/Home/FormSearchHotel'
 import ListHotel from '../Screens/Home/ListHotel'
+import FormSearchMobil from '../Screens/Home/FormSearchMobil'
 
 export default createStackNavigator(
     {
         Home,
         FormSearchHotel,
-        ListHotel
+        ListHotel,
+        FormSearchMobil
     },
     {
         headerMode: 'none',
