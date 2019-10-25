df04f13e698a12e3134e0518617ceda59a2035e2
Sutan Nasution <sutan.gnst@gmail.com>
2019-10-26 01:42:51 +0700

    add feature reset password

--- src/Screens/Account/ForgotPassword.js
+++ src/Screens/Account/ForgotPassword.js
@@ -1,10 +1,34 @@
 import React, { useState } from 'react'
 import { StyleSheet } from 'react-native'
-import { View, Text, Button, Icon, Item, Input } from 'native-base'
+import { View, Text, Button, Icon, Item, Input, Spinner, Toast } from 'native-base'
 import Header from '../../Components/Base/Header'
 import Color from '../../Assets/Color'
+import Http from '../../Helpers/Http'
 
 export default ({ navigation }) => {
+    const [email, setEmail] = useState('')
+    const [success, setSuccess] = useState()
+    const [loading, showLoading] = useState(false)
+
+    const resetPassword = () => {
+        showLoading(true)
+        Http.post('/users/reset-password', { email })
+            .then(({ data }) => {
+                setSuccess(`Sukses Mereset password, silahkan cek email anda di ${email.toLowerCase()}!`)
+            })
+            .catch(err => {
+                Toast.show({
+                    position: 'bottom',
+                    type: 'danger',
+                    text:
+                        err.message === 'Network Error'
+                            ? 'Tidak dapat menjangkau koneksi internet!'
+                            : err.response.data.message
+                })
+            })
+            .finally(() => showLoading(false))
+    }
+
     return (
         <>
             <Header
@@ -22,11 +46,19 @@ export default ({ navigation }) => {
                     mereset kata sandi baru.
                 </Text>
                 <Item regular style={styles.emailInput}>
-                    <Input placeholder="Email yang terdaftar" />
+                    <Input
+                        placeholder="Email yang terdaftar"
+                        onChangeText={value => setEmail(value)}
+                    />
                 </Item>
-                <Button block style={styles.btnReset}>
-                    <Text style={{ color: Color.Base }}>Reset Password</Text>
+                <Button block style={styles.btnReset} onPress={resetPassword}>
+                    {
+                        loading
+                            ? <Spinner color={Color.Base} />
+                            : <Text style={{ color: Color.Base }}>Reset Password</Text>
+                    }
                 </Button>
+                <Text style={styles.success}>{success}</Text>
             </View>
         </>
     )
@@ -44,5 +76,10 @@ const styles = StyleSheet.create({
         marginVertical: 15,
         borderRadius: 20,
         backgroundColor: Color.Primary
+    },
+    success: {
+        color: 'blue',
+        textAlign: 'center',
+        marginTop: 10
     }
 })
